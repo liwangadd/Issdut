be27d1b1ab75b87c9e349246d36b8e67437d137e
liwangadd <liwangadd@gmail.com>
2015-06-29 10:38:58 +0800

    modify something that i forgot

--- app/src/main/java/com/liwang/activity/DetailActivity.java
+++ app/src/main/java/com/liwang/activity/DetailActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewPropertyAnimator;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.AutoCompleteTextView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
